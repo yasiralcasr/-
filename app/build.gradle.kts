b80a231a5892f080b6e147b6a6abd355a28aabda
YASIRALRASHDI <yasiralcasr@gmail.com>
2026-08-30 05:26:42 +0300

    build: update application version to 2.0
    Bump versionCode to 2 and versionName to 2.0 to reflect the new release.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.aistudio.eastwestglobal.kxmpzq"
     minSdk = 24
     targetSdk = 36
-    versionCode = 1
-    versionName = "0.0.0.1"
+    versionCode = 2
+    versionName = "2.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
