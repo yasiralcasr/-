46d413cd400d00589961ff3fe10299d0ef06ff0e
YASIRALRASHDI <yasiralcasr@gmail.com>
2026-08-30 06:13:35 +0300

    build: bump version to 4.0
    Update version code to 4 and version name to 4.0 in build configuration.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.aistudio.eastwestglobal.kxmpzq"
     minSdk = 24
     targetSdk = 36
-    versionCode = 2
-    versionName = "2.0"
+    versionCode = 4
+    versionName = "4.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
